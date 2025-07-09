c828b3d5326a12c3b69d75d40cc33b669c26e836
Andrew <hello@example.com>
2025-07-09 21:34:50 +0400

    Added limit and offset to tasks list route.

--- tasks-api/list_router.py
+++ tasks-api/list_router.py
@@ -7,6 +7,6 @@ router = APIRouter()
 
 
 @router.get("/list")
-async def list_tasks(id: int | None = None):
-    tasks = await get_tasks(task_id=id)
+async def list_tasks(id: int | None = None, limit: int | None = None, offset: int = 0):
+    tasks = await get_tasks(task_id=id, limit=limit, offset=offset)
     return tasks
