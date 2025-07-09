b35761bde32f52a9f59bcc607e1cb30fbf7cc559
IchBinAndrew <158755519+IchBinAndrew@users.noreply.github.com>
2025-07-09 18:57:55 +0400

    Create test.py to show my new idea

---
+++ new-idea/test.py
@@ -0,0 +1,55 @@
+# TODO: Add logic for parsing Intel CPUs
+
+import enum
+
+class CategoryEnum(enum.Enum):
+    CONTENT = 1
+    GAMING = 2
+    WORK = 3
+
+class ModelEnum(str, enum.Enum):
+    INTEL = "Intel"
+    AMD = "AMD"
+
+
+data_input = "Ноут для видео, Asus VivoBook, Ryzen 3 3500U, 8 GB RAM, 1024 GB SSD"
+
+data = data_input.split(", ")
+data.pop(1)
+to_exclude_1 = ["видео", "игр", "работ"]
+for i in to_exclude_1:
+    if i in data[0]:
+        data[0] = CategoryEnum(to_exclude_1.index(i) + 1)
+        break
+to_exclude_2 = ["Ryzen ", "AMD Ryzen ", "Core ", "Intel Core "]
+model_index = -1
+model = None
+for i in to_exclude_2:
+    new_data_1 = data[1].removeprefix(i)
+    if new_data_1 != data[1]:
+        model_index = to_exclude_2.index(i)
+        if model_index < 2:
+            model = ModelEnum.AMD
+        else:
+            model = ModelEnum.INTEL
+        data[1] = new_data_1
+if model == ModelEnum.AMD:
+    for i in reversed(data.pop(1).split(" ")):
+        data.insert(1, i)
+    try:
+        data[1] = int(data[1])
+    except Exception:
+        print("Error")
+    try:
+        data[2] = int(data[2][:4])
+    except Exception:
+        print("Error")
+    try:
+        data[3] = int(data[3][:2])
+    except Exception:
+        print("Error")
+    try:
+        data[4] = int(data[4][:4])
+    except Exception:
+        print("Error")
+print(data)
